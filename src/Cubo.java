5953aa3f9c5b96a0290c2d5ecde145f69e1fb5a6
StevenCarrilloLoor <stevencarrilloloor@gmail.com>
2023-06-29 17:33:46 -0500

    Guardate bien

--- src/Cubo.java
+++ src/Cubo.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Cubo {
+public class Cubo extends Cuadrado {
+    @Override
+    public double calcularPerimetro() {
+        double perimetro = l * 12;
+        return perimetro;
+    }
+
+    @Override
+    public double calcularArea() {
+        double area = 6 * Math.pow(l, 2);
+        return area;
+    }
+
+    public double calcularVolumen() {
+        double volumen = Math.pow(l, 3);
+        return volumen;
+    }
 }
--- src/Esfera.java
+++ src/Esfera.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Esfera {
+public class Esfera extends Circulo {
+    @Override
+    public double calcularArea() {
+        double area = 4 * Math.PI * Math.pow(radio, 2);
+        return area;
+    }
+
+    public double calcularVolumen() {
+        double volumen = (4.0 / 3.0) * Math.PI * Math.pow(radio, 3);
+        return volumen;
+    }
 }
